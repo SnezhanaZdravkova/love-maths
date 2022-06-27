20192fb98c907aaf47415391cf43adbd39513201
Snezhana Zdravkova <snezhanazdravkova7@gmail.com>
2022-06-27 08:37:43 +0000

    Add change to files path and grammer mistake in parseInt

--- js/script.js
+++ js/script.js
@@ -68,8 +68,8 @@ function checkAnswer(){
  * directly from the DOM, and returns the correct answer.
  */
 function calculateCorrectAnswer(){
-   let operand1 = parceInt(document.getElementById("operand1").innerText);
-   let operand2 = parceInt(document.getElementById("operand2").innerText); 
+   let operand1 = parseInt(document.getElementById("operand1").innerText);
+   let operand2 = parseInt(document.getElementById("operand2").innerText); 
    let operator = document.getElementById("operator").innerText;
    if (operator === "+"){
     return[operand1 + operand2, "addition"];
